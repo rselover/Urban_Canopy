7dc4f1f6c2bb0febf72b506d25af270afc10a8c3
Rob Selover <rselover@users.noreply.github.com>
2026-04-25 07:19:57 -0400

    Fix formatting in loadTreeData function

--- data.js
+++ data.js
@@ -3,7 +3,8 @@ const geojsonUrl = './bprd_trees.json';
 function loadTreeData() {
   return d3.json(geojsonUrl).then(data => 
     data.features.map(feature => ({
-      position: [feature.geometry.coordinates[0], feature.geometry.coordinates[1]]
+      position: [feature.geometry.coordinates[0], feature.geometry.coordinates[1]],
+      properties: feature.properties
     }))
   );
 }
