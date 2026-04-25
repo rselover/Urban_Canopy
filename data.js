8ad9d1a635f5a49c930999498ffad4381abe453b
Rob Selover <rselover@users.noreply.github.com>
2026-04-25 06:13:14 -0400

    Update geojsonUrl with new expiration date

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const geojsonUrl = 'https://s3.amazonaws.com/og-production-open-data-bostonma-892364687672/resources/2f575489-e721-45ec-865a-e98f10d2ee85/bprd_trees.geojson?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJJIENTAPKHZMIPXQ%2F20260331%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260331T114803Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=a70c3a4b49c7a7b2a8262063ffded8ce6c757dfadc9828e99156b6016c349467';
+const geojsonUrl = 'https://s3.amazonaws.com/og-production-open-data-bostonma-892364687672/resources/2f575489-e721-45ec-865a-e98f10d2ee85/bprd_trees.geojson?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJJIENTAPKHZMIPXQ%2F20260425%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260425T101225Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=1d4452e31bb8e4881a98d1f47be13358b6aaad2ba80341702612bfa0131239de';
 
 function loadTreeData() {
   return d3.json(geojsonUrl).then(data => 
