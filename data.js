660e25a7e6012c993bf5e4cad299426b524e1d4b
Rob Selover <rselover@users.noreply.github.com>
2026-04-25 06:23:47 -0400

    Update geojsonUrl to local JSON file

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const geojsonUrl = 'https://s3.amazonaws.com/og-production-open-data-bostonma-892364687672/resources/2f575489-e721-45ec-865a-e98f10d2ee85/bprd_trees.geojson?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJJIENTAPKHZMIPXQ%2F20260425%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260425T101225Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=1d4452e31bb8e4881a98d1f47be13358b6aaad2ba80341702612bfa0131239de';
+const geojsonUrl = './brpd_trees.json';
 
 function loadTreeData() {
   return d3.json(geojsonUrl).then(data => 
